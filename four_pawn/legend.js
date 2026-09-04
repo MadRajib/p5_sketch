dc624e740b73cc188c53b8b0246515d4fe50f0fc
snehavramteke <snehavramteke358@gmail.com>
2026-09-04 21:18:43 +0530

    p5: 4_PAWN: Fix red errors

--- four_pawn/legend.js
+++ four_pawn/legend.js
@@ -45,6 +45,8 @@ const legendSketch = (p) => {
         p.text("To Move the Piece, Select \nthe Peice and then select the\nempty square.", 10, 240);
         p.fill(255, 0, 0);
         p.text("Reload to Reset the Game!", 10, 296);
+
+        p.noLoop();
     };
 
 };
